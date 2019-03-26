4a65c0f2f17003f309da606f19122e31342c58e0
Alberto Morales <cmtmorales24@gmail.com>
2019-03-26 22:19:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,11 @@ def execute_transaction
 end 
 
 def reverse_transfer 
-  
-
+   unless  @status == "reversed" 
+  @sender.balance += @amount 
+  @receiver.balance -= @amount
+  @status="reversed" 
+  end   
+end 
 
 end
