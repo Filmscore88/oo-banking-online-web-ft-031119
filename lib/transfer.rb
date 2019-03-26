6ac6b34f4285f769543bd81f4b409f8e70334e4e
Alberto Morales <cmtmorales24@gmail.com>
2019-03-26 21:48:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_accessor :status, :sender, :receiver, :amount
+
+def initialize(sender, receiver, transfer_amnt)
+@status= "pending"
+@sender= sender 
+@receiver= receiver 
+@amount= transfer_amnt
+end
+
+def valid?
+  @sender.valid? && @receiver.valid? 
+end 
+
+def execute_transaction 
+  unless  @status == "complete" 
+  @sender.balance -= @amount 
+  @receiver.balance += @amount
+     @status="complete"
+  end 
+end 
 end
